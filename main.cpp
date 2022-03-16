dfeba00ea87ff3a22794f80fddf51f4d3cf05f73
hllwrld7 <101736550+hllwrld7@users.noreply.github.com>
2022-03-16 17:48:54 +0100

    Added project files

---
+++ main.cpp
@@ -0,0 +1,9 @@
+
+#include "funcs.h"
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	startMenu(0);
+	return 0;
+}
